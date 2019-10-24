c839615bf48be278969094114d07a6acbb3bc8f5
Victoria Hale <vic.hale74@gmail.com>
2019-10-24 03:10:12 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,8 @@ class Backer
     @backed_projects = []
   end
   def back_project(project)
-    @project = project
-    @backed_projects << project
+    # @project = project
+    @backed_projects << @project
     
   end
  
