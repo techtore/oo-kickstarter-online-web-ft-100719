1b0aa1f7b7223a7dcc0b3678b38f0fd553b02800
Victoria Hale <vic.hale74@gmail.com>
2019-10-24 02:54:27 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,13 @@
 class Backer 
   attr_accessor :backed_projects
-  attr_reader :name
+  attr_reader :name, :project
   
   def initialize(name) 
     @name = name
+    @backed_projects = []
+  end
+  
+  def Project 
     @backed_projects = []
   end
 end
